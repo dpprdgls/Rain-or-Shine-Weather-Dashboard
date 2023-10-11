668287526ff8b1382abddd9ced74c7b3c68ee2db
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2023-10-10 17:31:57 -0700

    add css styling

--- assets/script.js
+++ assets/script.js
@@ -39,23 +39,82 @@ const apiKey = '165dd9ca68fdfa4ffbbf94d846a1293e';
             }
         }
 
+        // function displayWeatherForecast(data) {
+        //     const currentDayContainer = document.getElementById('currentDay');
+        //     const forecastDiv = document.getElementById('weather-forecast');
+        //     forecastDiv.innerHTML = '';
+            
+        //     let currentDate = new Date();
+
+        //     const currentDayForecast = data.list[0];
+        //     const currentDayContent = `
+        //     <h2>${forecastDate.toDateString()}</h2>
+        //     <p>Temperature: ${temperature}°F</p>
+        //     <p>Wind Speed: ${windSpeed} mph</p>
+        //     <p>Humidity: ${humidity}%</p>
+        //     <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">`;
+        //     currentDayContainer.innerHTML = currentDayContent;
+
+
+
+        //     for (let i = 1; i < 6; i++) {
+        //         const forecast = data.list[i];
+        //         // console.log(forecast);
+        //         const temperature = forecast.main.temp;
+        //         const windSpeed = forecast.wind.speed;
+        //         const humidity = forecast.main.humidity;
+        //         const weatherIcon = forecast.weather[0].icon;
+
+               
+
+        //         const forecastItem = document.createElement('div');
+        //         forecastItem.classList.add('forecast-item');
+        //         forecastItem.innerHTML = `
+        //             <h2>${forecastDate.toDateString()}</h2>
+        //             <p>Temperature: ${temperature}°F</p>
+        //             <p>Wind Speed: ${windSpeed} mph</p>
+        //             <p>Humidity: ${humidity}%</p>
+        //             <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
+        //         `;
+
+        //         forecastDiv.appendChild(forecastItem);
+        //     }
+        // }
+
         function displayWeatherForecast(data) {
+            const currentDayContainer = document.getElementById('currentDay');
             const forecastDiv = document.getElementById('weather-forecast');
             forecastDiv.innerHTML = '';
-
-            let currentDate = new Date();
-
-            for (let i = 0; i < 6; i++) {
+        
+            // Use the current date for the first day's forecast
+            const currentDayForecast = data.list[0];
+            const currentDate = new Date(currentDayForecast.dt * 1000);
+        
+            // Extract data for the first day
+            const temperature = currentDayForecast.main.temp;
+            const windSpeed = currentDayForecast.wind.speed;
+            const humidity = currentDayForecast.main.humidity;
+            const weatherIcon = currentDayForecast.weather[0].icon;
+        
+            const currentDayContent = `
+                <h2>${currentDate.toDateString()}</h2>
+                <p>Temperature: ${temperature}°F</p>
+                <p>Wind Speed: ${windSpeed} mph</p>
+                <p>Humidity: ${humidity}%</p>
+                <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">`;
+            currentDayContainer.innerHTML = currentDayContent;
+        
+            for (let i = 1; i <= 5; i++) {
                 const forecast = data.list[i];
-                // console.log(forecast);
+                // Calculate the date for each day based on the current date
+                const forecastDate = new Date(currentDate);
+                forecastDate.setDate(currentDate.getDate() + i);
+        
                 const temperature = forecast.main.temp;
                 const windSpeed = forecast.wind.speed;
                 const humidity = forecast.main.humidity;
                 const weatherIcon = forecast.weather[0].icon;
-
-                const forecastDate = new Date(currentDate);
-                forecastDate.setDate(currentDate.getDate() + i);
-
+        
                 const forecastItem = document.createElement('div');
                 forecastItem.classList.add('forecast-item');
                 forecastItem.innerHTML = `
@@ -65,7 +124,7 @@ const apiKey = '165dd9ca68fdfa4ffbbf94d846a1293e';
                     <p>Humidity: ${humidity}%</p>
                     <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
                 `;
-
+        
                 forecastDiv.appendChild(forecastItem);
             }
         }
